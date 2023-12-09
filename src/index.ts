c069fb502feb459a30d9b6a6d81e86e793fb8a97
Nabe847 <Nabe847@gmail.com>
2023-12-09 18:47:47 +0900

    update: json 返すように変更

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 
-const app = new Hono();
-app.get('/', (c) => c.text('Hello Hono!'));
+const app = new Hono().basePath('/api');
+app.get('/', (c) =>
+  c.json({
+    ok: true,
+    message: 'Hello Hono!',
+  }),
+);
 
 serve({ fetch: app.fetch, port: 3000 });
